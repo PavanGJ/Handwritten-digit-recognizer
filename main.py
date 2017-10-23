7ca8dd15d961d66ec4c7685dd2ae6aca3e4c861a
Pavan Joshi <pavan01121994@gmail.com>
2017-10-23 14:18:19 -0400

    Bug fixes
    Fixed a bug caused by importing a non-existent file

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 import logisticRegression
 import neuralNetwork
-import tensorFlow
 
 reg = logisticRegression.LogReg()
 reg.run()
 
 reg = neuralNetwork.NeuralNetwork()
-reg.run()
-
-reg = tensorFlow.TensorFlow()
 reg.run()
