649c121e790bf9e0db724b6cc1a828d76fc3356f
Pavan Joshi <pavan01121994@gmail.com>
2017-10-23 14:31:04 -0400

    Cleaning
    Cleaning the params file to remove any unnecessary parameters

--- params.py
+++ params.py
@@ -2,7 +2,6 @@
 #Purpose:   To declare global parameters that need to be changed for each traning set
 
 #PATHS TO FOLDERS
-USPS_FOLDER = './USPS_data'
 MNIST_TRAIN = './MNIST_data'
 MNIST_TEST = './MNIST_data'
 
@@ -17,14 +16,9 @@ NNREG = 0.0000
 #Precision Values
 PREC = 0.001
 
-#Activation Function Type
-#Possible values : 'RELU', 'IDENTITY', 'SOFTPLUS', 'TANH', 'SIGMOID'
-ACT = 'SIGMOID'
-
 #Output Function Type
 #Possible values : 'SOFTMAX', 'SIGMOID'
 OUT = 'SOFTMAX'
-
 
 #Input Dataset Split Ratio
 RATIO = 0.8
